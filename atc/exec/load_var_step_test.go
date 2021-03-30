bf3fce5e2ac564271e4a8d17d87026574a67fa3c
Aidan Oldershaw <aoldershaw@pivotal.io>
2021-03-30 11:33:26 -0400

    fix most breaking API changes
    there were a whole bunch of breaking changes, mostly renaming
    types/moving packages.
    
    the propagation.HTTPSupplier interface was renamed to
    propagation.TextMapCarrier, and got a new `Keys` method added. the
    Keys implementation for worker.ContainerSpec isn't technically correct -
    I left a comment explaining why
    
    the only thing holding us back from compiling is the honeycomb exporter,
    which is still on otel 0.16 (while we're trying to bump to the latest
    0.19).
    
    Signed-off-by: Aidan Oldershaw <aoldershaw@pivotal.io>

--- atc/exec/load_var_step_test.go
+++ atc/exec/load_var_step_test.go
@@ -9,7 +9,6 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/gbytes"
-	"go.opentelemetry.io/otel/api/trace"
 
 	"github.com/concourse/concourse/atc"
 	"github.com/concourse/concourse/atc/exec"
@@ -17,6 +16,7 @@ import (
 	"github.com/concourse/concourse/atc/exec/build/buildfakes"
 	"github.com/concourse/concourse/atc/exec/execfakes"
 	"github.com/concourse/concourse/atc/worker/workerfakes"
+	"github.com/concourse/concourse/tracing"
 )
 
 const plainString = "  pv  \n\n"
@@ -89,7 +89,7 @@ var _ = Describe("LoadVarStep", func() {
 		fakeDelegate.StderrReturns(stderr)
 
 		spanCtx = context.Background()
-		fakeDelegate.StartSpanReturns(spanCtx, trace.NoopSpan{})
+		fakeDelegate.StartSpanReturns(spanCtx, tracing.NoopSpan)
 
 		fakeDelegateFactory = new(execfakes.FakeBuildStepDelegateFactory)
 		fakeDelegateFactory.BuildStepDelegateReturns(fakeDelegate)
